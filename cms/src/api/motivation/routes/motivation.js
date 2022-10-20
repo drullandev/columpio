4eaddef16876b1a4b2d08f80e6707305f9b06b01
drullandev <drullan.dev@gmail.com>
2022-10-20 20:48:55 +0200

    Creating and configuring cms

---
+++ cms/src/api/motivation/routes/motivation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * motivation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::motivation.motivation');
---
+++ cms/src/api/motivation/services/motivation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * motivation service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::motivation.motivation');
